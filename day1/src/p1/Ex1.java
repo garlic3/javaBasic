8fc05d2ad36185905eebe3f005c998da221326e6
garlic3 <yup_yup@naver.com>
2022-03-02 13:32:35 +0900

    day1
    day1

---
+++ day1/src/p1/Ex1.java
@@ -0,0 +1,16 @@
+package p1;
+
+public class Ex1 {
+	public static void main(String[] args) {
+		System.out.println("Hello World!");
+		
+	}
+}
+/*
+main 매서드
+public 접근지정자
+static 객체를 생성하지 않고도 실행 가능
+void return이 없음을 의미
+main 자바는 반드시 1개의 main 매서드가 있어야함 시작점
+String[] 배열을 의미 args 	인수를 의미
+println -> 인쇄 후 줄바꿈 print -> 인쇄 후 줄 안바꿈 */
