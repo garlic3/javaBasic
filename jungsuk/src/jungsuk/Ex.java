9ba4f267cc10c82340a15be8e9301c70a47c67d6
garlic3 <yup_yup@naver.com>
2022-03-10 22:12:19 +0900

    .

--- jungsuk/src/jungsuk/Ex.java
+++ jungsuk/src/jungsuk/Ex.java
@@ -46,40 +46,85 @@ public class Ex {
 //		System.out.println("Celcius:"+celcius);
 //				
 //		
-		//4-2
-		int sum = 0;
-		for (int i = 1; i < 21; i++) {
-			if(i % 2 == 0 || i % 3 == 0) {
-				sum += i;
-			}
-		}
-		System.out.println("합계:"+sum);
+//		//4-2
+//		int sum = 0;
+//		for (int i = 1; i < 21; i++) {
+//			if(i % 2 == 0 || i % 3 == 0) {
+//				sum += i;
+//			}
+//		}
+//		System.out.println("합계:"+sum);
+//		
+//		//4-3
+//		sum = 0;
+//		for (int j = 1; j < 11; j++) {
+//			for(int k = 1; k < 11; k++) {
+//				sum += k;
+//			}
+//		}
+//		System.out.println("합계:"+sum);
+//		
+//		
+//		//4-4
+//
+//		sum = 0;
+//		int n;
+//		int m;
+//		for (n = 1, m = -2; ; n += 2, m -= -2) {
+//			sum = n + m;
+//			if(sum >= 100) {
+//				System.out.println("n:"+ n);
+//				System.out.println("m:"+ m);
+//				System.out.println("합계:" + sum);
+//				break;
+//			}
+//		}
+//		
+		//4-5
+//		int i = 0;
+//		while(i <= 10) {
+//			int j = 0;
+//			while(j < i) {
+//				System.out.print("*");
+//				j ++;
+//			}
+//			System.out.println();
+//			i ++;
+//		}
 		
-		//4-3
-		sum = 0;
-		for (int j = 1; j < 11; j++) {
-			for(int k = 1; k < 11; k++) {
-				sum += k;
-			}
-		}
-		System.out.println("합계:"+sum);
+		//4-6
+//		for (int i = 1; i < 7; i++) {
+//			for (int j = 1; j < 7; j++) {
+//				if (i + j == 6){
+//					System.out.println("i:"+i+" j:"+j);
+//				}
+//			}
+//		}
 		
+		//4-7
+//		String str = "12345";
+//		int sum = 0;
+//		for (int i = 0; i < str.length(); i++) {
+//			char ch = str.charAt(i);
+//			sum += (ch-'0');
+//		}
+//		System.out.println("sum=" + sum);		
 		
-		//4-4
 
-		sum = 0;
-		int n;
-		int m;
-		for (n = 1, m = -2; ; n += 2, m -= -2) {
-			sum = n + m;
-			if(sum >= 100) {
-				System.out.println("n:"+ n);
-				System.out.println("m:"+ m);
-				System.out.println("합계:" + sum);
-				break;
-			}
-		}
+		//4-8
+//		int value = (int)(Math.random()*6)+1;
+//		System.out.println("value:"+value);
+	
+
+		//4-9
+		int num = 12345;
+		int sum = 0;
 		
+		12345 / 10 = 1234
+				length - 1
+		for (int i = 0; i <= num.length(); )
+		
+		System.out.println("sum=" + sum);
 		
 	}
 
