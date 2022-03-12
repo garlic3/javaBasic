3c92abcb9f74657a7224f236370960d487b1c8db
garlic3 <yup_yup@naver.com>
2022-03-12 16:46:29 +0900

    day7

--- day7/src/day7/Ex1_2.java
+++ day7/src/day7/Ex1_2.java
@@ -3,8 +3,28 @@ package day7;
 public class Ex1_2 {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+/* package & import
+패키지는 비슷한 목적으로 생성된 클래스 파일들을 모아둔 폴더를 의미
+1개의 프로젝트에 1개 또는 여러개의 패키지를 생성할 수 있음
+패키지를 생성하면 클래스를 만들때 첫번째 줄에 반드시 'package 패키지명'이 명시되어야함
+패키지 사용의 장점: 클래스가 저장되는 공간이 분리되어 클래스명의 충돌을 방지
 
+import 
+패키지 선언 다음줄에 위치
+다른 패키지에 있는 클래스 사용방법 
+1. 클래스의 풀네임 사용(패키지명.클래스명)
+같은 패키지 내의 타 클래스 사용법 A a = new A()
+다른 패키지 내의 타 클래스 사용법 am.A a = new am.A()
+2. import 사용
+
+여러개 클래스를 import 
+->사실 import 되는 대상은 .java가 아니라 컴파일이 완료된 .class
+
+
+
+
+*/
+		
 	}
 
 }
