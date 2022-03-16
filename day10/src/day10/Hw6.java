e9a053de734070ff8bb981e015b50d315ca3c6a7
garlic3 <yup_yup@naver.com>
2022-03-16 23:24:30 +0900

    day10

--- day10/src/day10/Hw6.java
+++ day10/src/day10/Hw6.java
@@ -29,95 +29,95 @@ public class Hw6 {
 		}
 		
 		// 가장 뒤에 있는 요소 찾는 프로그램
-//		int j = a.length - 1;
-//		while(true) {
-//			if(a[j] == answer) {
-//				System.out.printf("그 값은 a[%s]에 있습니다 %n", j);
-//				break;
-//			}
-//			j--;
-//		}
+		int j = a.length - 1;
+		while(true) {
+			if(a[j] == answer) {
+				System.out.printf("그 값은 a[%s]에 있습니다 %n", j);
+				break;
+			}
+			j--;
+		}
 		
-//		// 6-11
-//		Arr arr = new Arr();
-//		int[] a = arr.makeArr();
-//		arr.inputArr(a, 10);
-//	
-//		int i = 1;
-//		while(i != a.length-1) {
-//			a[i] = (int)((Math.random()*10)+1);
-//			if (a[i] != a[i-1]) {
-//				i++;
-//			}
-//		}
-//		
-//		arr.printArr(a, "a");
+		// 6-11
+		Arr arr = new Arr();
+		int[] a = arr.makeArr();
+		arr.inputArr(a, 10);
+	
+		int i = 1;
+		while(i != a.length-1) {
+			a[i] = (int)((Math.random()*10)+1);
+			if (a[i] != a[i-1]) {
+				i++;
+			}
+		}
 		
-//		// 6-12
-//		Arr arr = new Arr();
-//		int[] a = arr.makeArr();
-//		arr.inputArr(a, 10);
-//		
-////		int i = 1;
-////		int count = 0;
-////		
-////		while(i < a.length) {
-////			for (int j = 0; j < i; j++) {
-////				if (a[j] == a[i]) {
-////					count++;
-////				}
-////			}
-////			if(count == 0) {
-////				i++;
-////			}
-////		}
-//		
-//		for(int i = 1; i < a.length; i++) {
-//			int j = 0;
-//			while(j < i) {
-//				if ( a[i] == a[j]) {
-//					a[i] = ((int)(Math.random()*10)+1);
-//				}
-//				j++;
-//				
-//			}
-//		}
-//		arr.printArr(a, "a");
+		arr.printArr(a, "a");
+		
+		// 6-12
+		Arr arr = new Arr();
+		int[] a = arr.makeArr();
+		arr.inputArr(a, 10);
+		
+		int i = 1;
+		int count = 0;
+		
+		while(i < a.length) {
+			for (int j = 0; j < i; j++) {
+				if (a[j] == a[i]) {
+					count++;
+				}
+			}
+			if(count == 0) {
+				i++;
+			}
+		}
+		
+		for(int i = 1; i < a.length; i++) {
+			int j = 0;
+			while(j < i) {
+				if ( a[i] == a[j]) {
+					a[i] = ((int)(Math.random()*10)+1);
+				}
+				j++;
+				
+			}
+		}
+		arr.printArr(a, "a");
 		
 		// 6-13
-//		Arr arr = new Arr();
-//		int[] a = arr.makeArr();
-//		arr.inputArr(a, 100);
-//		arr.printArr(a, "a");
-//		
-//		System.out.println("요소를 섞었습니다");
-//		
-//		for (int i = 0; i < a.length; i++) {
-//			int k = (int)(Math.random()*a.length);
-//			int tem = a[i];
-//			a[i] = a[k];
-//			a[k] = tem;
-//		}
-//		arr.printArr(a,"a");
+		Arr arr = new Arr();
+		int[] a = arr.makeArr();
+		arr.inputArr(a, 100);
+		arr.printArr(a, "a");
 		
-//		// 6-14
-//		Arr arr = new Arr();
-//		int[] a = arr.makeArr();
-//		arr.inputArr(a, 15);
-//		arr.printArr(a, "a");
-//		
-//		int[] b = Arrays.copyOf(a, a.length);
-//			
-//		for (int c = 1; c < b.length; c++) {
-//			for (int d = 0; d < c; d++) {
-//				if ( b[d] > b[c]) {
-//					int tem = b[c];
-//					b[c] = b[d];
-//					b[d] = tem;
-//				}
-//			}
-//		}
-//		arr.printArr(b, "b");
+		System.out.println("요소를 섞었습니다");
+		
+		for (int i = 0; i < a.length; i++) {
+			int k = (int)(Math.random()*a.length);
+			int tem = a[i];
+			a[i] = a[k];
+			a[k] = tem;
+		}
+		arr.printArr(a,"a");
+		
+		// 6-14
+		Arr arr = new Arr();
+		int[] a = arr.makeArr();
+		arr.inputArr(a, 15);
+		arr.printArr(a, "a");
+		
+		int[] b = Arrays.copyOf(a, a.length);
+			
+		for (int c = 1; c < b.length; c++) {
+			for (int d = 0; d < c; d++) {
+				if ( b[d] > b[c]) {
+					int tem = b[c];
+					b[c] = b[d];
+					b[d] = tem;
+				}
+			}
+		}
+		arr.printArr(b, "b");
 		
 			
 			
