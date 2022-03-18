43b2bc40788d0994ebda13c06b65afd2f0fb9e3c
garlic3 <yup_yup@naver.com>
2022-03-18 18:49:51 +0900

    day12

--- day12/src/day12/Ex1.java
+++ day12/src/day12/Ex1.java
@@ -16,39 +16,49 @@ public class Ex1 {
 	    // 81
 	    // 짝                                                   짝짝      100
 		
-	
+		Check check = new Check();
+		
 		for (int i = 1; i < 101; i++) {
 			int count = 0;
-			String result = i + "\t";
+			String snum = i + "\t";
 			
-			 
-			// 10보다 크고 3,6,9로 끝나면
 			
-			if (i > 10 && (result.charAt(1) == '3'||result.charAt(1) == '6'||result.charAt(1) == '9' )) {
-				count ++;
+			// 10보다 작은 수에서 3,6,9의 개수 세기
+			if (i > 10) {
+				count = check.checkThree(snum);
 			}
-			// 10보다 크고 3,6,9로 시작하면
-			if (i > 10 && (result.charAt(0) == '3'||result.charAt(0) == '6'||result.charAt(0) == '9')) {
-				count ++;
-			}
-			// 숫자에 3,6,9개 2개라면 짝짝 1개라면 짝
-			result = (count == 1) ? "짝\t" : (count == 2) ? "짝짝\t" : i + "\t";
+
+			snum = (count == 1) ? "짝\t" : (count == 2) ? "짝짝\t" : i + "\t";
 
 			// 10의 배수라면 줄바꿈  
 			if (i % 10 == 0) {
-				result += "\n";
+				snum += "\n";
 			}
 			// 10보다 작고 3의 배수라면
-			if (i < 10 && i % 3 == 0) {
-				result = "짝\t";
+			else if (i < 10 && i % 3 == 0) {
+				snum = "짝\t";
 			}
-			System.out.print(result);
+			System.out.print(snum);
 			
 		}
 		
 		
-		
-		
 	}
 
+}
+
+class Check {
+		int checkThree(String snum) {
+			int count = 0;
+			// 첫번째 자리에 3,6,9가 있다면 count = 1 
+			if (snum.charAt(0) == '3'||snum.charAt(0) == '6'||snum.charAt(0) == '9') {
+				count++;
+			}
+			// 두번째 자리에 3,6,9가 있다면 count = 2
+			if (snum.charAt(1) == '3'||snum.charAt(1) == '6'||snum.charAt(1) == '9') {
+				count++;
+			}
+			return count;
+		}
+		
 }
